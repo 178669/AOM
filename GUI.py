4db2a83d8b90145ba468f013860dc9cbd9c9ba47
David <zdavidbusiness@gmail.com>
2023-11-12 14:09:23 -0500

    Donate Page Updates

--- GUI.py
+++ GUI.py
@@ -25,7 +25,7 @@ class startApp(tk.Tk):
         self.frames = {}
 
         #loop for frame/page initialization
-        for F in (StartPage, ChoicePage, DonatePage):
+        for F in (StartPage, ChoicePage, DonatePage, TokenPage):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -43,7 +43,7 @@ class StartPage(tk.Frame):
         self.rowconfigure([0, 1, 2, 3, 4, 5, 6, 7, 8], minsize=0, weight=1)
         self.columnconfigure([0, 1, 2, 3, 4, 5, 6, 7, 8], minsize=0, weight=1) #weight allows for resizing
 
-        b_startimg = tk.PhotoImage(file='images/b_pink.png')
+        b_startimg = tk.PhotoImage(file='Images/b_pink.png')
 
         #b_start = tk.Button(self, text="Start a Trade", width=200, command=lambda:controller.show_frame(Page1))
         b_start = tk.Button(self, image=b_startimg, command=lambda: controller.show_frame(ChoicePage), borderwidth=0, bg="white", activebackground="white")
@@ -68,13 +68,13 @@ class ChoicePage(tk.Frame):
         self.rowconfigure([0, 1, 2, 3, 4, 5], minsize=50, weight=0)
         self.columnconfigure([0, 1, 2, 3], minsize=50, weight=0)  # weight allows for resizing
 
-        b_donateimg = tk.PhotoImage(file='images/b_donateo50.png')
-        b_shopimg = tk.PhotoImage(file='images/b_shop.png')
-        b_tradeimg = tk.PhotoImage(file='images/b_trade.png')
+        b_donateimg = tk.PhotoImage(file='Images/b_donateo50.png')
+        b_shopimg = tk.PhotoImage(file='Images/b_shop.png')
+        b_tradeimg = tk.PhotoImage(file='Images/b_trade.png')
 
-        img1 = tk.PhotoImage(file='images/oneimg.png')
-        img2 = tk.PhotoImage(file='images/twoimg.png')
-        img3 = tk.PhotoImage(file='images/threeimg.png')
+        img1 = tk.PhotoImage(file='Images/oneimg.png')
+        img2 = tk.PhotoImage(file='Images/twoimg.png')
+        img3 = tk.PhotoImage(file='Images/threeimg.png')
 
         imglab1 = Label(self, image=img1, borderwidth=0)
         imglab2 = Label(self, image=img2, borderwidth=0)
@@ -84,9 +84,9 @@ class ChoicePage(tk.Frame):
         imglab2.photo=img2
         imglab3.photo=img3
 
-        imglab1.grid(row=0, column=0)
-        imglab2.grid(row=2, column=0)
-        imglab3.grid(row=4, column=0)
+        imglab1.grid(row=0, column=0, pady=10)
+        imglab2.grid(row=2, column=0, pady=10)
+        imglab3.grid(row=4, column=0, pady=10)
 
         b_donate = tk.Button(self, image=b_donateimg, command=lambda: controller.show_frame(DonatePage), borderwidth=0, bg="white", activebackground="white")
         b_donate.photo = b_donateimg
@@ -113,7 +113,41 @@ class ChoicePage(tk.Frame):
 class DonatePage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        self.configure(bg="white")
+        self.rowconfigure(6, weight=1)
+        self.columnconfigure(0, weight=1)
 
+        b_home = tk.Button(self, text="HOME")
+        b_home.grid(row=0, column=0, sticky="ne")
+
+        donatetitle = tk.Label(self, text="DONATE", font=('Gloucester MT Extra Condensed', 275), fg="pink", bg="white")
+        donatetitle.grid(row=0, column=0)
+
+        donatetext = tk.Label(self, text="Place Item In Donation Box Below", font=('Gloucester MT Extra Condensed',100), fg="red", bg="white")
+        donatetext.grid(row=1,column=0)
+
+
+        fill1=LabelFrame(self, height=30)
+        fill1.grid(row=5, column=0)
+
+        arrimg = tk.PhotoImage(file='Images/downarrow.png')
+        arrlab = Label(self, image=arrimg, bg="white")
+        arrlab.photo = arrimg
+        arrlab.grid(row=4, column=0)
+
+        doneimg = tk.PhotoImage(file='Images/b_done.png')
+        b_done = tk.Button(self, image=doneimg, command=lambda: controller.show_frame(TokenPage), borderwidth=0, bg="white", activebackground="white")
+        b_done.photo = doneimg
+        b_done.grid(row=6, column=0)
+
+class TokenPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(bg="white")
+        self.rowconfigure(5, weight=1)
+        self.columnconfigure(0, weight=1)
+        tokentitle = tk.Label(self, text="Please Tap Your ID to Receive Your Token", font=('Gloucester MT Extra Condensed', 100), fg="pink", bg="white")
+        tokentitle.grid(row=0, column=0)
 
 
 """
