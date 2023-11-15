17dc8056c6259e0e283f8c8911f4226efaf788fc
178669 <zdavidbusiness@gmail.com>
2023-11-15 00:41:37 -0500

    Goodnight

--- SerialControl.py
+++ SerialControl.py
@@ -25,7 +25,26 @@ def controlmotor():
     except Exception as e:
         print(e)
         arduino.close()
+items = [1, 2, 3, 4, 5, 6]
+#fucntion to determine if CW or CCW is faster
+def calc(filepath, target, max):
+    file = open(filepath, "r")
+    current = f.read()
+    
+    if (current == 5 && target == 1):
+        CW = true
+        return 1
 
-def calc():
+    elif (current == 6 && target == 1)||(current == 6 && target ==2):
+        CW = true
+        return target
+
+    
+    if max - current >= 3:
+        CW = true
+        return max - current
+
+
+
 
 def 
