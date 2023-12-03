bd7f0a24e3bbb7c0191777f18b2c7756d4f22fe2
David <zdavidbusiness@gmail.com>
2023-12-03 18:57:06 -0500

    Sync

--- scratch.py
+++ scratch.py
@@ -1,39 +1,34 @@
-def __init__(self, main):
-    # canvas for image
-    self.canvas = Canvas(main, width=60, height=60)
-    self.canvas.grid(row=0, column=0)
-
-    # images
-    self.my_images = []
-    self.my_images.append(PhotoImage(file="ball1.gif"))
-    self.my_images.append(PhotoImage(file="ball2.gif"))
-    self.my_images.append(PhotoImage(file="ball3.gif"))
-    self.my_image_number = 0
-
-    # set first image on canvas
-    self.image_on_canvas = self.canvas.create_image(0, 0, anchor='nw', image=self.my_images[self.my_image_number])
-
-    # button to change image
-    self.button = Button(main, text="Change", command=self.onButton)
-    self.button.grid(row=1, column=0)
-
-
-# ----------------
-
-def onButton(self):
-    # next image
-    self.my_image_number += 1
-
-    # return to first image
-    if self.my_image_number == len(self.my_images):
-        self.my_image_number = 0
-
-    # change image
-    self.canvas.itemconfig(self.image_on_canvas, image=self.my_images[self.my_image_number])
-
-def animate(self):
-    if self._image_id is None:
-        self._image_id = self.display.create_image(...)
-    else:
-        self.itemconfig(self._image_id, image= the_new_image)
-    self.display.after(self.gif["delay"],
+import tkinter as tk
+from PIL import Image, ImageTk
+from itertools import count, cycle
+class ImageLabel(tk.Label):
+    """
+    A Label that displays images, and plays them if they are gifs
+    :im: A PIL Image instance or a string filename
+    """
+    def load(self, im):
+        if isinstance(im, str):
+            im = Image.open(im)
+        frames = []
+        try:
+            for i in count(1):
+                frames.append(ImageTk.PhotoImage(im.copy()))
+                im.seek(i)
+        except EOFError:
+            pass
+        self.frames = cycle(frames)
+        try:
+            self.delay = im.info['duration']
+        except:
+            self.delay = 100
+        if len(frames) == 1:
+            self.config(image=next(self.frames))
+        else:
+            self.next_frame()
+    def unload(self):
+        self.config(image=None)
+        self.frames = None
+    def next_frame(self):
+        if self.frames:
+            self.config(image=next(self.frames))
+            self.after(self.delay, self.next_frame)
